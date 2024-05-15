dd83fa26e689acad6736e10b370552bd83da25f7
pjbRPF <peter.bell@raspberrypi.org>
2024-05-15 13:08:30 +0100

    take a photo

--- picamzero/Camera.py
+++ picamzero/Camera.py
@@ -1,4 +1,7 @@
 from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
+from time import sleep
 
 class Camera():
 
@@ -12,7 +15,23 @@ class Camera():
         """    
         self._pc2 = Picamera2()
         self.demo_string = demo_string
+        
+    def record_video(self, filename, duration=5):
+        
+        if not filename.endswith('.mp4'):
+            filename += '.mp4'  # Append '.mp4' extension if not present
 
-
+        # Start recording the video
+        video_config = self._pc2.create_video_configuration()   # Do we need a config option?
+        self._pc2.configure(video_config)
+        self._pc2.start(show_preview=True)
+        encoder = H264Encoder(10000000)
+        
+        output = FfmpegOutput(filename)
+        self._pc2.start_recording(encoder, output)
+        sleep(duration)
+        self._pc2.stop_recording()
+        self._pc2.stop_preview()
 
 cam = Camera()
+cam.record_video('test', 10)
