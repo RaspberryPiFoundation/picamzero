ee414b86aac8585a27942a44944613742a116f90
lawsie <laura.sach@raspberrypi.org>
2024-08-02 12:12:04 +0100

    Update test formatting

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -31,32 +31,41 @@ def cam():
 
 
 # ----------------------------------
-# Camera.py tests
+# Initialise camera
 # ----------------------------------
-
 
 # Initialise a camera
 def test_init(cam: Camera):
     assert cam.pc2 is not None
 
+# ----------------------------------
+# Camera orientation (hflip/vflip)
+# ----------------------------------
+
+
+
+# ----------------------------------
+# Video
+# ----------------------------------
 
 # Record a video with a specific filename
 def test_named_video(cam: Camera):
     cam.record_video("testvideo.mp4", 3)
     assert exists("testvideo.mp4")
 
-
 # Record a video with a specific filename
 def test_named_video_no_extension(cam):
     cam.record_video("testvid", 3)
     assert exists("testvid.mp4")
-
 
 # Fail to specify a filename for a video
 def test_unnamed_video(cam):
     with pytest.raises(PicameraZeroException):
         _ = cam.record_video()
 
+# ----------------------------------
+# Image
+# ----------------------------------
 
 # Take a picture with a specific filename
 def test_named_picture(cam):
@@ -93,12 +102,14 @@ def test_named_picture_no_ext(cam):
     assert exists(filename)
     assert exists(filename2)
 
+# ----------------------------------
+# Sequence
+# ----------------------------------
 
 # Fail to specify a filename for a sequence
 def test_unnamed_sequence(cam):
     with pytest.raises(PicameraZeroException):
         cam.capture_sequence()
-
 
 # Test a sequence capture with a filename but no extension
 def test_named_sequence_no_extension(cam):
@@ -106,28 +117,11 @@ def test_named_sequence_no_extension(cam):
     assert exists("test-0.jpg")
     assert exists("test-9.jpg")
 
-
-# Can you take a video and stills
-def test_video_with_stills(cam):
-    cam.take_video_and_still(filename="abc", duration=12, still_interval=2)
-    assert exists("abc.mp4")
-    assert exists("abc-0.jpg")
-    assert exists("abc-5.jpg")
-    assert not exists("abc-6.jpg")
-
-    cam.take_video_and_still(filename="testvs")
-    assert exists("testvs.mp4")
-    assert exists("testvs-0.jpg")
-    assert exists("testvs-4.jpg")
-    assert not exists("testvs-5.jpg")
-
-
 # Test a named sequence capture with extension
 def test_named_sequence(cam):
     cam.capture_sequence("testing.jpg")
     assert exists("testing-0.jpg")
     assert exists("testing-9.jpg")
-
 
 # Test whether you can change the number of pics
 def test_sequence_quantity(cam):
@@ -135,7 +129,6 @@ def test_sequence_quantity(cam):
     assert exists("fewer-0.jpg")
     assert exists("fewer-1.jpg")
     assert not exists("fewer-2.jpg")
-
 
 # Test the sequence interval
 def test_sequence_interval(cam):
@@ -150,8 +143,26 @@ def test_sequence_interval(cam):
     elapsed2 = stop2 - start2
     assert elapsed > elapsed2
 
-
 # Test the video gets made when you do a sequence
 def test_sequence_with_video(cam):
     cam.capture_sequence(filename="with-vid", make_video=True)
     assert exists("with-vid-timelapse.mp4")
+    
+
+# ----------------------------------
+# Video and still
+# ----------------------------------
+
+# Can you take a video and stills
+def test_video_with_stills(cam):
+    cam.take_video_and_still(filename="abc", duration=12, still_interval=2)
+    assert exists("abc.mp4")
+    assert exists("abc-0.jpg")
+    assert exists("abc-5.jpg")
+    assert not exists("abc-6.jpg")
+
+    cam.take_video_and_still(filename="testvs")
+    assert exists("testvs.mp4")
+    assert exists("testvs-0.jpg")
+    assert exists("testvs-4.jpg")
+    assert not exists("testvs-5.jpg")
