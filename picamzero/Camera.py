b0f6c55832e3bb28573a1912ac8b646063b43c93
lawsie <laura.sach@raspberrypi.org>
2024-08-13 10:53:32 +0100

    Add tests and make camera always started

--- picamzero/Camera.py
+++ picamzero/Camera.py
@@ -477,7 +477,8 @@ class Camera:
         if remaining_time > 0:
             sleep(remaining_time)
 
-        self.pc2.stop_recording()
+        self.stop_recording()
+        self.pc2.start()
 
     # Take a picture
     def take_photo(self, filename=None):
@@ -494,6 +495,8 @@ class Camera:
 
         # Capture the image
         self.pc2.start_and_capture_file(name=filename)
+
+        self.pc2.start()
 
         # Useful to know what the file is called
         return filename
@@ -547,9 +550,10 @@ class Camera:
                         )
 
                 video.release()
-                return video_name
+
             except Exception as e:
                 return f"Error creating video: {e}"
+        self.pc2.start()  # Restart camera
 
     # Record a video
     def record_video(self, filename=None, duration=5):
@@ -560,7 +564,7 @@ class Camera:
         self.pc2.start_and_record_video(
             filename, config=self._generate_config("VIDEO"), duration=duration
         )
-
+        self.pc2.start()
         return filename
 
     # Record a video with option to take a photo
@@ -583,3 +587,4 @@ class Camera:
         Stop recording video
         """
         self.pc2.stop_recording()
+        self.pc2.start()
