520085d7aed298add3f837aecfcd1fa02b42291c
lawsie <laura.sach@raspberrypi.org>
2024-08-13 10:55:22 +0100

    Add tests (whoops)

---
+++ tests/test_started_state.py
@@ -0,0 +1,80 @@
+import pytest
+from picamzero import Camera
+
+
+@pytest.fixture(autouse=True)
+def cwd(tmpdir, monkeypatch):
+    """
+    This fixture changes the current working directory before
+    each test in this file to to a temporary directory so that
+    image / video clean up is taken care of by the OS.
+    """
+    monkeypatch.chdir(tmpdir)
+
+
+# Returns a camera to use in tests
+@pytest.fixture
+def cam():
+    camera = Camera()
+    yield camera
+    camera.pc2.close()
+
+
+# ----------------------------------
+# Test started state retained
+# ----------------------------------
+
+
+def test_started_flip(cam: Camera):
+    cam.flip_camera(hflip=True, vflip=True)
+    assert cam.pc2.started is True
+
+
+def test_started_preview_start(cam: Camera):
+    cam.start_preview()
+    assert cam.pc2.started is True
+
+
+def test_started_preview_stop(cam: Camera):
+    cam.stop_preview()
+    assert cam.pc2.started is True
+
+
+def test_started_annotate(cam: Camera):
+    cam.annotate("text")
+    assert cam.pc2.started is True
+
+
+def test_started_video_and_still(cam: Camera):
+    cam.take_video_and_still("vstill", duration=6, still_interval=2)
+    assert cam.pc2.started is True
+
+
+def test_started_take_photo(cam: Camera):
+    cam.take_photo("single_photo")
+    assert cam.pc2.started is True
+
+
+def test_started_capture_image(cam: Camera):
+    cam.capture_image("synonym")
+    assert cam.pc2.started is True
+
+
+def test_started_capture_sequence(cam: Camera):
+    cam.capture_sequence("sequence", num_images=3)
+    assert cam.pc2.started is True
+
+
+def test_started_record_video(cam: Camera):
+    cam.record_video("video")
+    assert cam.pc2.started is True
+
+
+def test_started_start_recording(cam: Camera):
+    cam.start_recording("videostart")
+    assert cam.pc2.started is True
+
+
+def test_started_stop_recording(cam: Camera):
+    cam.stop_recording()
+    assert cam.pc2.started is True
