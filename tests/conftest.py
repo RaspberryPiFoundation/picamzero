e99c4939b52735453f7ac218f315e960595e0260
Geraint Ballinger <G3zz@users.noreply.github.com>
2024-08-22 17:53:20 +0100

    Fix DRM issue (#187)
    * Fix DRM issue
    
    Hardcode NULL preview while we use a Raspberry Pi OS
    Lite machine as the self-hosted runner
    
    * Fix no such variable
    
    * Another attempt
    
    I'm unable to test this locally, so we'll see what CI (Simon?) says.
    
    * Fix typeerror

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 # Provide the path to the module so that the tests can run
 import os
 import sys
+import types
 
 import pytest
 
@@ -27,6 +28,19 @@ def cam():
     Camera._instance_count = 0
 
     camera = Camera()
+
+    # Override picamera2's start_preview method to ensure
+    # that the NULL preview is always used during testing
+    # by saving a reference to the original function and
+    # calling it with preview always set to False
+    original_start_preview_function = camera.pc2.start_preview
+
+    def overridden_start_preview(self, preview=False, **kwargs):
+        preview = False
+        return original_start_preview_function(preview, **kwargs)
+
+    camera.pc2.start_preview = types.MethodType(overridden_start_preview, camera.pc2)
+
     try:
         yield camera
     finally:
